fac11431164d8ca6bfc025c793de43cf4337d935
Adrian Suarez <asuarez9382@gmail.com>
2023-10-30 21:22:41 -0700

    complete lab

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const num2 = 2;
 const num3 = 10;
 const num4 = 6;
 
-let multiply = num1*num2
+const multiply = num1*num2
 
-let random = Math.floor(Math.random()*10);
+const random = Math.floor(Math.random()*10);
 
-let mod = num3%num4
+const mod = num3%num4
 
-let max = Math.max(1, 2, 3, 4, 20);
+const max = Math.max(1, 2, 3, 4, 20);
